7954c144f3e3cee009f4a1e4f68c444d183e483e
Irina Shulman <irena@one1.co.il>
2023-02-27 10:36:18 +0200

    test_rep_commit_1

---
+++ 2-intro.js
@@ -0,0 +1,8 @@
+
+console.log(__filename
+    );
+
+setInterval(()=>{
+    console.log('Hello!')
+},1000)
+//console.log(__dirname)
---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+//common js
+//modules
+const names = require('./Modules1-names')
+const sayHi = require('./Modules2-utils')
+const data = require('./6-altrntv_flvrs')
+require('./7-mind-grenade')
+sayHi('Susan')
+sayHi(names.john)
+sayHi(names.peter)
